72c0ae94607b0d2d1e81cafceeb949fc7a6e4edd
joelcapel <jcapel@elpuig.xeill.net>
2022-01-24 11:57:39 +0100

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,24 @@ package com.company;
 
 public class Main {
 
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("Hola mundo!");
+        System.out.println("ñdjssdfkjbsd");
+    }
+
+    public void hola(){ //Metodo para sacar nombre y numero
+        String nom = "Joel "; //nombre
+        int num = 2; // numero
+
+        System.out.println(nom + num); //salida del nombre y numero juntos
+
+        /**
+         * Creamos un metodo
+         * Introducimos variables
+         * Las imprimimos
+         */
     }
 }
